669090896beee59c592e7cb43262670b228ecace
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-09-29 17:58:35 +0200

    task1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+
+
+const listUl = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+    (elem, indx, arr) => {
+        return ` <li class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      data-source="${elem.original}"
+      alt="${elem.description}"
+    />
+ </a>
+</li>`
+    }
+).join("");
+listUl.insertAdjacentHTML("afterbegin", markup);
+
+listUl.addEventListener("click", selectImg);
+
+function selectImg(evt) {
+    evt.preventDefault();
+    if (evt.target === evt.currentTarget) { return };
+
+const instance = basicLightbox.create(`
+    <img src= '${evt.target.dataset.source}'>
+`)
+
+  instance.show()    
+ document.addEventListener("keydown", event => {
+  if (event.code !== "Escape") {
+      return;
+  }
+   instance.close(); 
+});    
+}
+
+
+
+
+console.log(galleryItems);
